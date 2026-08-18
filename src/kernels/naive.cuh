ee1890fec0962080dac23ea97c10c9942eb93e36
Brandon <b1luu@ucsd.edu>
2026-08-17 18:49:30 -0700

    coalesced solution

--- src/kernels/naive.cuh
+++ src/kernels/naive.cuh
@@ -1,6 +1,6 @@
 __global__ void matMult(const float* A, const float*B, float*C, float alpha, float beta, int M, int N, int K) { 
-    int row  = blockIdx.y * blockDim.y + threadIdx.y;
-    int col  = blockIdx.x * blockDim.x + threadIdx.x;
+    int col  = blockIdx.y * blockDim.y + threadIdx.y;
+    int row  = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (row >= M || col >= N) return;
 
